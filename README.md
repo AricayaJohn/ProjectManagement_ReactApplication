07fae6cf5c31e7cf9bc5cfa7c6dc3d31956f0eee
John <jaricaya@gmail.com>
2024-07-28 07:07:08 -0700

    updated ReadMe

--- README.md
+++ README.md
@@ -1,70 +1,32 @@
-# Getting Started with Create React App
+# Project Management React Application
+## Overview
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a Project Management application built using React. It allows users to manage employees, and their projects. The application includes functionalities such as adding new employees, viewing and updating employee projects.
 
-## Available Scripts
+### Features
 
-In the project directory, you can run:
+Employee Management: Add new employees with details such as name, image, job title, and work status.
 
-### `npm start`
+Project Updates: Real-time updates of projects and tasks
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### Installation
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Clone the Repository: https://github.com/AricayaJohn/ProjectManagement_ReactApplication
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Install Dependencies
 
-### `npm run build`
+npm install
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### Run the Backend
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+In the terminal enter json-server --watch db.json
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Usage
 
-### `npm run eject`
+Add Employees: Navigate to the employee section and fill out the form to add new employees.
+View Tasks: Click on an View Tasks inside employee card to view their projects.
+Add Tasks: Use the task form to add new tasks for an employee.
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Contact
+ Linkedin - https://www.linkedin.com/in/john-aricaya/
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,80 +1,80 @@
-import React, {useEffect, useState} from "react";
+// import React, {useEffect, useState} from "react";
 
-function ToDoList({tasks}) {
-    const [currentTasks, setCurrentTasks] = useState([])
-   const [newTask, setNewTask] = useState("")
+// function ToDoList({tasks}) {
+//     const [currentTasks, setCurrentTasks] = useState([])
+//    const [newTask, setNewTask] = useState("")
 
-    useEffect(()=> {
-        setCurrentTasks(tasks)
-    }, [tasks])
+//     useEffect(()=> {
+//         setCurrentTasks(tasks)
+//     }, [tasks])
 
-function handleInputChange( event ){
-    setNewTask(event.target.value);
-}
-function addTask(){
-    if (newTask.trim() !== ""){
-        setCurrentTasks(t => [...t, newTask]);
-        setNewTask("");
-    }
-}
-function deleteTask( index ){
-    const updatedTasks = currentTasks.filter((_, i) => i !== index);
-    setCurrentTasks(updatedTasks);
-}
-function moveTaskup( index ){
-    if (index > 0) {
-        const updatedTasks = [...currentTasks];
-       [updatedTasks[index], updatedTasks[index -1]] = 
-       [updatedTasks[index -1], updatedTasks[index]];
-       setCurrentTasks(updatedTasks);
-    }
-}
-function moveTaskDown( index ){
-    if (index < tasks.length -1) {
-        const updatedTasks = [...currentTasks];
-       [updatedTasks[index], updatedTasks[index +1]] = 
-       [updatedTasks[index +1], updatedTasks[index]];
-       setCurrentTasks(updatedTasks);
-    }
-}
+// function handleInputChange( event ){
+//     setNewTask(event.target.value);
+// }
+// function addTask(){
+//     if (newTask.trim() !== ""){
+//         setCurrentTasks(t => [...t, newTask]);
+//         setNewTask("");
+//     }
+// }
+// function deleteTask( index ){
+//     const updatedTasks = currentTasks.filter((_, i) => i !== index);
+//     setCurrentTasks(updatedTasks);
+// }
+// function moveTaskup( index ){
+//     if (index > 0) {
+//         const updatedTasks = [...currentTasks];
+//        [updatedTasks[index], updatedTasks[index -1]] = 
+//        [updatedTasks[index -1], updatedTasks[index]];
+//        setCurrentTasks(updatedTasks);
+//     }
+// }
+// function moveTaskDown( index ){
+//     if (index < tasks.length -1) {
+//         const updatedTasks = [...currentTasks];
+//        [updatedTasks[index], updatedTasks[index +1]] = 
+//        [updatedTasks[index +1], updatedTasks[index]];
+//        setCurrentTasks(updatedTasks);
+//     }
+// }
 
-    return (
-    <div className = "to-do-list"> 
-        <h1> 
-            Daily To-Do List
-        </h1>
-        <div>
-            <input  type = "text"
-                    placeholder = "Enter a Task..."
-                    value = {newTask}
-                    onChange = {handleInputChange} />
-            <button 
-                    className = "add-button"
-                    onClick = {addTask}>
-                ADD
-            </button>
-        </div>
-        <ol>
-            {currentTasks.map((task, index) => 
-                <li key = {index}>
-                    <span className = "text"> {task}</span>
-                    <button className = "delete-button"
-                            onClick = {() => deleteTask(index)}>
-                        Delete
-                    </button>
-                    <button className = "move-button"
-                            onClick = {() => moveTaskup(index)}>
-                        Move up ☝🏽
-                    </button>
-                    <button className = "move-button"
-                            onClick = {() => moveTaskDown(index)}>
-                        Move down 👇🏽
-                    </button>
-                </li>
-            )}
-        </ol>
-    </div>
-    )
-}
+//     return (
+//     <div className = "to-do-list"> 
+//         <h1> 
+//             Daily To-Do List
+//         </h1>
+//         <div>
+//             <input  type = "text"
+//                     placeholder = "Enter a Task..."
+//                     value = {newTask}
+//                     onChange = {handleInputChange} />
+//             <button 
+//                     className = "add-button"
+//                     onClick = {addTask}>
+//                 ADD
+//             </button>
+//         </div>
+//         <ol>
+//             {currentTasks.map((task, index) => 
+//                 <li key = {index}>
+//                     <span className = "text"> {task}</span>
+//                     <button className = "delete-button"
+//                             onClick = {() => deleteTask(index)}>
+//                         Delete
+//                     </button>
+//                     <button className = "move-button"
+//                             onClick = {() => moveTaskup(index)}>
+//                         Move up ☝🏽
+//                     </button>
+//                     <button className = "move-button"
+//                             onClick = {() => moveTaskDown(index)}>
+//                         Move down 👇🏽
+//                     </button>
+//                 </li>
+//             )}
+//         </ol>
+//     </div>
+//     )
+// }
 
-export default ToDoList
+// export default ToDoList
